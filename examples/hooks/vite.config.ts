b720ffa257526708cc383a67021c8d345bbf9d25
Shubham Mathur <itseasy21@gmail.com>
2025-08-07 23:22:21 +1000

    vite and package updates

--- examples/hooks/vite.config.ts
+++ examples/hooks/vite.config.ts
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { resolve } from 'path';
-
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => ({
+export default defineConfig({
   build: {
     target: ['safari11.1', 'chrome64', 'firefox66', 'edge88'],
     outDir: resolve(__dirname, './dist'),
-    input: { main: resolve(__dirname, 'index.html') },
     assetsDir: '',
     emptyOutDir: true,
   },
@@ -16,4 +14,10 @@ export default defineConfig(() => ({
     open: true,
     port: 8002,
   },
-}));
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'leaflet', 'react-leaflet']
+  },
+  esbuild: {
+    target: 'es2020'
+  }
+});
